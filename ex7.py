6417dadb1e7d9df0a24dea6e2ff123e579f80aef
Alan <alaurit@gmail.com>
2016-07-14 18:35:21 -0500

    refactored to print variables mary and lamb

--- ex7.py
+++ ex7.py
@@ -1,5 +1,10 @@
-print "Mary had a little lamb."
-print "It's fleece was white as %s." % 'snow'
+lamb = "little lamb"
+mary = "Mary had a %s," % lamb
+
+print mary
+print "%s, %s." % (lamb, lamb)
+print mary
+print "it's fleece was white as %s." % 'snow'
 print "And everywhere that Mary went."
 print "." * 10 # what'd that do?
 
@@ -15,7 +20,8 @@ end9 = "r"
 end10 = "g"
 end11 = "e"
 end12 = "r"
+end13 = "!"
 
 # watch that comma at the end. try removing it to see what happens
 print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 + end10 + end11 + end12
+print end7 + end8 + end9 + end10 + end11 + end12 + end13
