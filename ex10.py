83ca148c913319a81c5324dab1ed96ff6aa22f5f
Alan <alaurit@gmail.com>
2016-07-15 10:11:12 -0500

    added string formatting

--- ex10.py
+++ ex10.py
@@ -1,13 +1,17 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
+food = "Cat food"
+fish = "Fishies"
+nip = "Catnip"
+grass = "Grass"
 
-fat_cat = """
+fat_cat = '''
 I'll do a list:
-\t* Cat food
-\t* Fishies
-\t* Catnip\n\t* Grass
-"""
+\t* %r
+\t* %r
+\t* %r\n\t* %r
+''' % (food, fish, nip, grass)
 
 print tabby_cat
 print persian_cat
