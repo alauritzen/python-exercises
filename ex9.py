ee3345b4f76196c9f92f9a58c2c1df4c450cb2c3
Alan <alaurit@gmail.com>
2016-07-15 09:25:47 -0500

    added variables to try inserting them in block of text. Must be on same line as end quotes.

--- ex9.py
+++ ex9.py
@@ -1,12 +1,15 @@
 days = "Mon Tue Wed Thu Fri Sat Sun"
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #\nSep\nOct\nNov\nDec"
+something = "something"
+double_quotes = "double-quotes"
+how_many = 4
 
 print "Here are the days: ", days
 print "Here are the months: ", months
 
 print """
-There's something going on here.
-With the three double quotes.
+There's %s going on here.
+With the three %s.
 We'll be able to type as much as we like.
-Even 4 lines if we want, or 5, or 6.
-"""
+Even %d lines if we want, or %d, or %d.
+""" % (something, double_quotes, how_many, how_many+1, how_many+2)
